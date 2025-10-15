1c5aade04bc6a24ad89742f84798381358dcb374
amulyapaul <rohitvarmamuthyam123@gmail.com>
2025-10-15 16:42:35 +0530

    2nd commit

---
+++ ec2.tf
@@ -0,0 +1,14 @@
+resource "aws_instance" "web" {
+    ami = "ami-0b982602dbb32c5bd"
+    instance_type = "t2.micro"
+    key_name = "maven430"
+    tags = {
+    Name = "rohit"
+    }
+  
+}
+
+provider "aws" {
+    region = "us-east-1"
+    profile = "default"
+  }
